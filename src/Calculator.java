651a0a098f9da565008f2cdde9f3ec7839f39974
whatamido <42977140+whatamido@users.noreply.github.com>
2018-09-17 14:28:22 -0400

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator 
+{
+	public int add(int num1, int num2)
+	{
+	int ans = num1 + num2;
+	return ans;	
+	}
+	
+}
---
+++ src/CalculatorMain.java
@@ -0,0 +1,11 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) 
+	{
+		Calculator calc = new Calculator();
+		
+		System.out.println(calc.add(2,4));
+	}
+
+}
